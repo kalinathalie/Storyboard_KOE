16318e353dc43e2d32172ddf999b2621c2bfb122
Kali Nathalie <kalinathalie@outlook.com>
2020-02-25 00:28:24 -0300

    add first script

---
+++ Background.cs
@@ -0,0 +1,33 @@
+﻿using StorybrewCommon.Scripting;
+using StorybrewCommon.Storyboarding;
+using System.Linq;
+
+namespace StorybrewScripts
+{
+    public class Background : StoryboardObjectGenerator
+    {
+        [Configurable]
+        public string BackgroundPath = "";
+
+        [Configurable]
+        public int StartTime = 0;
+
+        [Configurable]
+        public int EndTime = 0;
+
+        [Configurable]
+        public double Opacity = 0.2;
+
+        public override void Generate()
+        {
+            if (BackgroundPath == "") BackgroundPath = Beatmap.BackgroundPath ?? string.Empty;
+            if (StartTime == EndTime) EndTime = (int)(Beatmap.HitObjects.LastOrDefault()?.EndTime ?? AudioDuration);
+
+            var bitmap = GetMapsetBitmap(BackgroundPath);
+            var bg = GetLayer("").CreateSprite(BackgroundPath, OsbOrigin.Centre);
+            bg.Scale(StartTime, 480.0f / bitmap.Height);
+            bg.Fade(StartTime - 500, StartTime, 0, Opacity);
+            bg.Fade(EndTime, EndTime + 500, Opacity, 0);
+        }
+    }
+}
