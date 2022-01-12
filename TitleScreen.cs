3157b63e9406be7a458c80caa9d2e27664ced821
Walavouchey <36758269+Walavouchey@users.noreply.github.com>
2022-01-12 05:26:33 +0100

    keep credits visible for a bit longer

--- TitleScreen.cs
+++ TitleScreen.cs
@@ -13,6 +13,12 @@ namespace StorybrewScripts
     {
         //Makes K1L4 titles screen at the beginning of the map
 
+        [Configurable]
+        public int StartTime = 13;
+
+        [Configurable]
+        public int EndTime = 1897;
+
         private StoryboardLayer layer;
         public override void Generate()
         {
@@ -49,27 +55,27 @@ namespace StorybrewScripts
             var oneSprite = layer.CreateSprite(one.Path,OsbOrigin.TopCentre);
 
             setupTitle(kSprite,484, new Vector2(265,240));
-            setupTitle(fourSprite,484+50, new Vector2(305,240));
+            setupTitle(fourSprite,485+50, new Vector2(305,240));
             setupTitle(lSprite,484+100, new Vector2(345,240));
             setupTitle(oneSprite,484+150, new Vector2(375,240));
 		    // Vertical bar and mask
             var mask = layer.CreateSprite("sb/pixel.png",OsbOrigin.TopCentre);
-            mask.Scale(13,400);
-            mask.Fade(13,1583,1,1);
-            mask.Color(13,Color4.Black);
+            mask.Scale(StartTime,400);
+            mask.Fade(StartTime, 484+150+250,1,1);
+            mask.Color(StartTime,Color4.Black);
 
             var line = layer.CreateSprite("sb/pixel.png");
-            line.ScaleVec(13,327,0,0,1,1);
+            line.ScaleVec(StartTime,327,0,0,1,1);
             line.ScaleVec(OsbEasing.InOutCirc,327,641,1,1,150,1);
-            line.Fade(13,1583-200,1,1);
-            line.Fade(1583-200,1583,1,0);
+            line.Fade(StartTime,EndTime-200,1,1);
+            line.Fade(EndTime-200,EndTime,1,0);
 
             //Presents
 
             var presents = layer.CreateSprite("sb/presents.png",OsbOrigin.Centre,new Vector2(320,260));
             presents.Scale(641,0.2);
             presents.Fade(641,641+200,0,1);
-            presents.Fade(1583-200,1583,1,0);
+            presents.Fade(EndTime-200,EndTime,1,0);
 
             
 
@@ -79,9 +85,9 @@ namespace StorybrewScripts
         // Makes the letters appear one by one
         public void setupTitle(OsbSprite sprite, int time, Vector2 position)
         {
-            sprite.Scale(13,0.6);
-            sprite.Fade(13,1583-200,1,1);
-            sprite.Fade(1583-200,1583,1,0);
+            sprite.Scale(StartTime,0.6);
+            sprite.Fade(StartTime,EndTime-200,1,1);
+            sprite.Fade(EndTime-200,EndTime,1,0);
             sprite.Move(OsbEasing.OutCirc, time, time+250, position, position + new Vector2(0,-50));
 
         }
